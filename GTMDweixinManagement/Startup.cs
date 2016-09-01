c159100a2422b7acb38900ea3be8acba8e06c98d
tianmaoyu <tianmaoyu@foxmail.com>
2016-09-01 15:44:20 +0800

    添加项目文件。

---
+++ GTMDweixinManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GTMDweixinManagement.Startup))]
+namespace GTMDweixinManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
