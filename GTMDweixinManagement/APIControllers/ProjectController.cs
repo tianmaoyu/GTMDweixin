adb2dce7c11cbcb56c727e9db6dd400c9c51f0d1
tianmaoyu <tianmaoyu@foxmail.com>
2016-09-14 16:59:19 +0800

    加入：project

---
+++ GTMDweixinManagement/APIControllers/ProjectController.cs
@@ -0,0 +1,80 @@
+﻿using GTMDweixinManagement.BLL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace GTMDweixinManagement.APIControllers
+{
+    public class ProjectController : ApiController
+    {
+        public object ProjectBLL { get; private set; }
+
+        // Post: api/Project/GetList
+        [HttpPost]
+        public JArray GetList()
+        {
+            ProjectBLL ProjectBLL = new ProjectBLL();
+            return ProjectBLL.GetTree();
+        }
+
+        // Post: api/Project/Add
+        [HttpPost]
+        public string Add(JObject pagerParas)
+        {
+            var id = HttpContext.Current.Request.QueryString["id"];
+            //var form = HttpContext.Current.Request.Form;
+
+            ProjectBLL ProjectBLL = new ProjectBLL();
+            if (id != null && Int32.Parse(id) > 0)
+            {
+                ProjectBLL.Updata(pagerParas, Int32.Parse(id));
+            }
+            else
+            {
+                try
+                {
+                    pagerParas.Remove("ID");
+                }
+                catch (Exception ex)
+                {
+
+                }
+                ProjectBLL.Add(pagerParas);
+            }
+            return "1";
+        }
+
+        // DELETE: api/Project/Delete/5
+        [HttpPost]
+        public JObject Delete(int id)
+        {
+            
+            ProjectBLL ProjectBLL = new ProjectBLL();
+            var deleteSuccess = ProjectBLL.Delete(id);
+            JObject result = new JObject();
+            if (deleteSuccess == 1)
+            {
+                result["success"] = true;
+            }
+            else
+            {
+                result["errorMsg"] = "删除失败！";
+            }
+            return result;
+        }
+
+
+        public JObject Edit(JObject pagerParas)
+        {
+            ProjectBLL ProjectBLL = new ProjectBLL();
+            ProjectBLL.Add(pagerParas);
+            return new JObject();
+        }
+    }
+}
---
+++ GTMDweixinManagement/BLL/ProjectBLL.cs
@@ -0,0 +1,87 @@
+﻿using GTMDweixinManagement.EF;
+using LinqKit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace GTMDweixinManagement.BLL
+{
+    public class ProjectBLL
+    {
+        private GTMDweixinEntities db = null;
+        public ProjectBLL()
+        {
+            db = new GTMDweixinEntities();
+        }
+
+        public JObject GetInfo(int id)
+        {
+            JObject result = new JObject();
+            if (id > 0)
+            {
+                var info = db.ProjectInfoes.Where(item => item.ID == id).FirstOrDefault();
+                if (info != null)
+                {
+                    result = JObject.Parse(JsonConvert.SerializeObject(info));
+                }
+            }
+            return result;
+        }
+
+
+        public JArray GetTree()
+        {
+            JArray jArray = new JArray();
+            var listInfos = db.ProjectInfoes.ToList();
+            JArray children = JArray.Parse(JsonConvert.SerializeObject(listInfos));
+            JObject rootNode = new JObject();
+            rootNode["id"] = -2;
+            rootNode["text"] = "全部菜单";
+            rootNode["state"] = "open";
+            rootNode["children"] = children;
+            jArray.Add(rootNode);
+            return jArray;
+        }
+
+
+
+        //新增
+        public int Add(JObject json)
+        {
+            ProjectInfo projectInfo = JsonConvert.DeserializeObject<ProjectInfo>(json.ToString());
+            db.Insert(projectInfo);
+            return 1;
+        }
+        /// <summary>
+        /// 编辑
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int Updata(JObject json, int id)
+        {
+            ProjectInfo projectInfo = JsonConvert.DeserializeObject<ProjectInfo>(json.ToString());
+            projectInfo.ID = id;
+            var _ProjectInfo = db.ProjectInfoes.AsNoTracking().FirstOrDefault(item => item.ID == id);
+            if (projectInfo != null)
+            {
+                db.Updata(projectInfo);
+            }
+            return 1;
+        }
+
+        public int Delete(int id)
+        {
+            var ProjectInfo = db.ProjectInfoes.FirstOrDefault(item => item.ID == id);
+            if (ProjectInfo != null)
+            {
+                db.Delete(ProjectInfo);
+            }
+            return 1;
+        }
+
+    }
+}
