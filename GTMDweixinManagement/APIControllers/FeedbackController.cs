a8288850d6a703c8b4dcc7bb3a939fa4ea03bdb2
tianmaoyu <tianmaoyu@foxmail.com>
2016-09-21 15:45:35 +0800

    加入反馈

---
+++ GTMDweixinManagement/APIControllers/FeedbackController.cs
@@ -0,0 +1,63 @@
+﻿using GTMDweixinManagement.BLL;
+using GTMDweixinManagement.EF;
+using LinqKit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace GTMDweixinManagement.APIControllers
+{
+    public class FeedbackController : ApiController
+    {
+        //public object NoticeBll { get; private set; }
+
+        // Post: api/User/GetList
+        [HttpPost]
+        public JObject GetList(JObject pagerParas)
+        {
+            FeedbackBLL feedbackBll = new FeedbackBLL();
+            return feedbackBll.GetList(pagerParas);
+        }
+
+       
+
+        // DELETE: api/User/Delete/5
+        [HttpPost]
+        public JObject Delete(JObject json)
+        {
+            var ids= JsonConvert.DeserializeObject<List<int>>(json["ids"].ToString());
+            FeedbackBLL feedbackBll = new FeedbackBLL();
+            var deleteSuccess= feedbackBll.Delete(ids);
+            JObject result = new JObject();
+            if (deleteSuccess == 1)
+            {
+                result["success"] = true;
+            }
+            else
+            {
+                result["errorMsg"] = "删除失败！";
+            }
+            return result;
+        }
+
+       
+        [HttpPost]
+        public JArray GetInfo()
+        {
+            JArray result = new JArray();
+            FeedbackBLL feedbackBll = new FeedbackBLL();
+
+            var infos = JsonConvert.SerializeObject(feedbackBll.GetAll());
+            result = JArray.Parse(infos);
+            return result;
+        }
+       
+    }
+}
---
+++ GTMDweixinManagement/BLL/FeedbackBLL.cs
@@ -0,0 +1,67 @@
+﻿using EntityFramework.Extensions;
+using GTMDweixinManagement.EF;
+using LinqKit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace GTMDweixinManagement.BLL
+{
+    public class FeedbackBLL
+    {
+       private  GTMDweixinEntities db = null;
+       public FeedbackBLL()
+       {
+            db = new GTMDweixinEntities();
+       }
+
+        public JObject GetList(JObject pagerParas)
+        {
+            JObject json = new JObject();
+            var PageIndex = Int32.Parse(pagerParas["page"].ToString());
+            var PageSize = Int32.Parse(pagerParas["rows"].ToString());
+            var queryName = pagerParas["name"];
+            var queryNunmber= pagerParas["phoneNumber"];
+
+            var predicate = PredicateBuilder.True<FeedbackInfo>();
+            var total = db.FeedbackInfoes.Where(predicate.Compile()).ToList().Count;
+            var userInfos = db.FeedbackInfoes.Where(predicate.Compile()).ToList().Skip((PageIndex - 1) * PageSize).Take(PageSize);
+            var rows= JArray.Parse(JsonConvert.SerializeObject(userInfos));
+            json["rows"] = rows;
+            json["total"] = total;
+            return json;
+        }
+       
+        public int Delete(int id)
+        {
+            var noticeInfo = db.NoticeInfoes.FirstOrDefault(item => item.ID == id);
+            if (noticeInfo != null)
+            {
+                db.Delete(noticeInfo);
+            }
+            return 1;
+        }
+        public int Delete(List<int> ids)
+        {
+            
+            Expression<Func<NoticeInfo, bool>> conditions = item=> ids.Contains(item.ID);
+            db.DeleteBulk<NoticeInfo>(conditions);
+            return 1;
+        }
+
+        /// <summary>
+        /// 得到所有的用户信息
+        /// </summary>
+        /// <returns></returns>
+        public List<NoticeInfo> GetAll()
+        {
+            List<NoticeInfo> noticeInfoes = new List<NoticeInfo>();
+            noticeInfoes = db.NoticeInfoes.ToList();
+            return noticeInfoes;
+        }
+    }
+}
