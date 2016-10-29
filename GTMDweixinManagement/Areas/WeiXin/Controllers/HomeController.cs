6817cd23e0b451371d126e2d637a30cce4a8bb7d
tianmaoyu <tianmaoyu@foxmail.com>
2016-10-29 11:06:39 +0800

    加入home 页面

---
+++ GTMDweixinManagement/Areas/WeiXin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTMDweixinManagement.Areas.WeiXin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: WeiXin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
