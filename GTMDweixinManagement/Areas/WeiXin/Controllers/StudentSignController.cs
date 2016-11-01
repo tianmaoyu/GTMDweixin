6e4fbeca1816859232b346a822a264be7e197937
tianmaoyu <tianmaoyu@foxmail.com>
2016-11-01 19:36:34 +0800

    加入，签到，报名

---
+++ GTMDweixinManagement/Areas/WeiXin/Controllers/StudentSignController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTMDweixinManagement.Areas.WeiXin.Controllers
+{
+    public class StudentSignController : Controller
+    {
+        // GET: WeiXin/StudentSign
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WeiXin/StudentSign/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WeiXin/StudentSign/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: WeiXin/StudentSign/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+    }
+}
