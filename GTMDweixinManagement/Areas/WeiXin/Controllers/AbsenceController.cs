7926ee3a343eb8040259b54e99dc990f3535150c
tianmaoyu <tianmaoyu@foxmail.com>
2016-12-03 17:15:33 +0800

    请假模板修改

--- GTMDweixinManagement/Areas/WeiXin/Controllers/AbsenceController.cs
+++ GTMDweixinManagement/Areas/WeiXin/Controllers/AbsenceController.cs
@@ -25,65 +25,6 @@ namespace GTMDweixinManagement.Areas.WeiXin.Controllers
         {
             return View();
         }
-
-        // POST: WeiXin/Absence/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: WeiXin/Absence/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: WeiXin/Absence/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: WeiXin/Absence/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: WeiXin/Absence/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+     
     }
 }
